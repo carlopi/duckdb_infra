0cb1d2fce1df2a4a9028add52bb490bc8b55c98a
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-06-29 08:49:14 +0200

    Update extensions.py to test also community

--- extensions.py
+++ extensions.py
@@ -5,8 +5,10 @@ import time
 print(duckdb.__version__)
 
 extension_list = ['aws', 'icu', 'fts', 'spatial', 'httpfs', 'tpch', 'tpcds', 'arrow', 'azure', 'excel', 'iceberg', 'inet', 'mysql', 'postgres', 'sqlite', 'sqlsmith', 'vss']
+community_extension_list = ['h3', 'scrooge', 'sqlite_scanner', 'quack']
 
 to_be_tested = random.sample(extension_list, 5)
+to_be_tested_community = random.sample(community_extension_list, 3)
 
 print('extension_name', 'install_time', 'load_time')
 for ext in to_be_tested:
@@ -20,6 +22,14 @@ for ext in to_be_tested:
 for ext in to_be_tested:
     a = time.perf_counter()
     duckdb.install_extension(ext)
+    b = time.perf_counter()
+    duckdb.load_extension(ext)
+    c = time.perf_counter()
+    print(ext, b-a, c-b)
+
+for ext in to_be_tested_community:
+    a = time.perf_counter()
+    duckdb.sql("INSTALL " + ext + " FROM community").all()
     b = time.perf_counter()
     duckdb.load_extension(ext)
     c = time.perf_counter()
